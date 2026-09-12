af61d615706e661b1522f613f1b79f48739e3dd2
x2 <lunguj665@gmail.com>
2026-09-12 18:18:40 +0200

    added  arrays for X and the y

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import subprocess
 import pathlib
 import cv2
+import numpy
 
 # subprocess.run(["./split"])
 # buffer
@@ -8,8 +9,16 @@ BUFFER = []
 
 frames = pathlib.Path("frames")
 frame_number = 1
+
 for i in range(100):
-    frame = cv2.imread(f"dist/frame_{frame_number:06d}.png")
+    frame = cv2.imread(f"dist/frame_{frame_number:06d}.png", 0)
     BUFFER.append(frame)
     frame_number += 1
-print(BUFFER[0])
+
+# inputs
+dataset = numpy.array(BUFFER[0])
+m, n = dataset.shape
+Xp = dataset[:, 0]
+Yp = dataset[:, 1:]
+
+print(Xp)
