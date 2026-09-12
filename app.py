31b20776e0ce2c90d9559853647c173bfee727f4
x2 <lunguj665@gmail.com>
2026-09-13 00:56:40 +0200

    added a cnn

--- app.py
+++ app.py
@@ -1,34 +1,51 @@
-import subprocess
-import pathlib
 import cv2
-import numpy
+import numpy as np
 
-# subprocess.run(["./split"])
-# buffer
+# storage of a 100 images
 BUFFER = []
-
-frames = pathlib.Path("frames")
-frame_number = 1
-
 for i in range(100):
-    frame = cv2.imread(f"dist/frame_{frame_number:06d}.png", 0)
-    BUFFER.append(frame)
-    frame_number += 1
-
+    frame = cv2.imread(f"dist/frame_{i + 1:06d}.png", 0)
+    if frame is not None:
+        resized_frame = cv2.resize(frame, (28, 28))
+        BUFFER.append(resized_frame)
 # inputs
-dataset = numpy.array(BUFFER[0])
-m, n = dataset.shape
-Xp = dataset[:, 0]
-Yp = dataset[:, 1:]
+X = np.array(BUFFER).reshape(100, 1, 28, 28)
+X = X / 255.0
 
-print(Xp)
-# layers
+# layer factory
 
 
-class LayerDense:
-    def __init__(self, n_input, n_neurons):
-        self.weights = 0.01 * numpy.random.randn(n_input, n_neurons)
-        self.biases = numpy.zeros((1, n_neurons))
+class LayerConvolution:
+    def __init__(self, n_filters, filter_size):
+        self.n_filters = n_filters
+        self.filter_size = filter_size
+        self.weights = 0.01 * np.random.randn(n_filters, filter_size, filter_size)
+        self.biases = np.zeros((n_filters, 1))
 
     def forward(self, inputs):
-        self.output = numpy.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+        n_samples, channels, h, w = inputs.shape
+        out_h = h - self.filter_size + 1
+        out_w = w - self.filter_size + 1
+
+        self.output = np.zeros((n_samples, self.n_filters, out_h, out_w))
+
+        for i in range(out_h):
+            for j in range(out_w):
+                image_patch = inputs[
+                    :, :, i : i + self.filter_size, j : j + self.filter_size
+                ]
+
+                for f in range(self.n_filters):
+                    self.output[:, f, i, j] = (
+                        np.sum(image_patch * self.weights[f], axis=(1, 2, 3))
+                        + self.biases[f]
+                    )
+
+
+conv1 = LayerConvolution(n_filters=8, filter_size=3)
+
+conv1.forward(X)
+
+print(f"Original image shape: {X.shape}")
+print(f"Convolution output shape: {conv1.output.shape}")
